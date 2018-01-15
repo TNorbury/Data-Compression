6336aa5d392e17938766e9c88189d10cf88d4402
TNorbury <tylernorbury96@gmail.com>
2018-01-15 14:28:48 -0800

    Only compress repeating values
    The algorithm will now only "squish" repeating value of equal value. It will now also round values that fall within a certain upper or lower range

--- dataCompression.py
+++ dataCompression.py
@@ -5,8 +5,7 @@ import random
 def main():
    oldData = 0
    oldDataTime = datetime.datetime.now()
-   numChunks = 0
-   compressionThreshold = 32767 
+   repeatingValues = 0
 
    # Open the file to write data to
    dataFile = open("data", "w")
@@ -14,30 +13,37 @@ def main():
    # Indefintely read data in (from SPI, or just a num generator)
    while (True):
       # Generate a random value (this will be SPI in the real deal)
-      data = random.randint(0x0, 0xFFFF)
+      data = round(random.uniform(-0.5, 5.5), 1)
       dataTime = datetime.datetime.now()
 
-      # If the difference between the new data and the last data read 
-      # (or the average of chunks read) is greater than the amount we set for 
-      # compression, then we want to write it to the file
-      if (math.fabs(data - oldData) > compressionThreshold):
-         # Don't write to the file if this is the first data being read
-         if (numChunks != 0):
-            dataLog = str(numChunks) + " " + str(oldData) + " " + str(oldDataTime.strftime("%I:%M:%S.%f"))         
-            dataFile.write(dataLog + "\n")
+      # If the data is between either the upper or lower range, then we want 
+      # to set data to the respective value in order to eliminate minor 
+      # deviations when the signal is either high or low. 
+      if (data >= -0.1 and data <= 0.1):
+         data = 0
+      elif (data >= 4.9 and data <= 5.1):
+         data = 5
 
-         # We're starting on a new chunk of data, with the current data serving as the starting point
-         numChunks = 1
+      # If the current data is equal the previously acquired data, then we want
+      # to increase the counter of repeating values
+      if (data == oldData):
+         repeatingValues += 1
+
+      # Otherwise, if the current data is equal to the last data, then we want
+      # to write the previous data to the file and set the previous data to 
+      # our current data
+      elif (data != oldData):
+         # If this is the first data that's been read, then don't write 
+         # anything to the file
+         if (repeatingValues != 0):
+            dataFile.write(str(repeatingValues) + " " + str(oldData) + " " 
+            + oldDataTime.strftime("%I:%M:%S.%f") + "\n")
+
+         # Reset the number of repeating values, and set the current data as the old data
+         repeatingValues = 1
          oldData = data
          oldDataTime = dataTime
 
-      # Otherwise, if the difference is too small, or there isn't any old data,
-      # then average old data with data
-      elif (math.fabs(data - oldData) <= compressionThreshold):
-         # Adjust the average size of the chunk
-         oldData = (oldData * numChunks) + data
-         numChunks += 1
-         oldData = oldData // numChunks
 
 if __name__ == "__main__":
    main()
