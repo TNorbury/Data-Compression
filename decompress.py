53801fa8fee9d3af8c5a138e4fd1a56a1b7ee9b0
Tyler Norbury <tylernorbury96@gmail.com>
2018-01-22 11:24:52 -0800

    Program now reads compressed data and outputs decompressed data

---
+++ decompress.py
@@ -0,0 +1,31 @@
+import sys
+
+def main():
+    # The first argument will be the name of the (compressed) data file, while
+    # second will be the output file
+    compressedFilePath = sys.argv[1]
+    decompressedFilePath = sys.argv[2]
+
+    compressedFile = open(compressedFilePath, "r")
+    decompressedFile = open(decompressedFilePath, "w")
+
+    # Iterate over each line in the file
+    for line in compressedFile:
+        # Split the line into three parts: number of repeating values, the data,
+        # and the time the data was read
+        lineSplit = line.split()
+        repeatingValues = int(lineSplit[0])
+        data = lineSplit[1]
+        dataTime = lineSplit[2]
+
+        # Now for each of the repeating values, write the data, along with the
+        # timestamp, to the file
+        for i in range(0, repeatingValues):
+            decompressedFile.write(data + "," + dataTime + "\n")
+
+    compressedFile.close()
+    decompressedFile.close()
+
+
+if __name__ = "__main__":
+    main()
