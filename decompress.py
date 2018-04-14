c99d26488914fd6ef38f47de794b3d900843afd3
Tyler Norbury <tylernorbury96@gmail.com>
2018-04-14 10:54:35 -0700

    Changed the decompression alogorithm
    It should now work with the new way we're compressing data

--- decompress.py
+++ decompress.py
@@ -1,33 +1,52 @@
 import sys
 
 def main():
-    # The first argument will be the name of the (compressed) data file, while
-    # second will be the output file
-    compressedFilePath = sys.argv[1]
-    decompressedFilePath = sys.argv[2]
+   # The first argument will be the name of the (compressed) data file, while
+   # second will be the output file
+   compressedFilePath = sys.argv[1]
+   decompressedFilePath = sys.argv[2]
 
-    compressedFile = open(compressedFilePath, "r")
-    decompressedFile = open(decompressedFilePath, "w")
+   compressedFile = open(compressedFilePath, "r")
+   decompressedFile = open(decompressedFilePath, "w")
 
-    # Iterate over each line in the file
-    for line in compressedFile:
-        # Split the line into three parts: number of repeating values, the data,
-        # and the time the data was read
-        lineSplit = line.split()
+   repeatingValues = False
 
-        # If the line didn't have 3 sections, then don't write anything
-        if (len(lineSplit) == 3):
-            repeatingValues = int(lineSplit[0])
+   # Iterate over each line in the file
+   for line in compressedFile:
+
+      # If the line is "-R-" that means that the next line in the file is 
+      # repeating values
+      if (line == "-R-"):
+         repeatingValues = True
+      
+      # Otherwise, if the line doesn't contain repeating values, then just 
+      # print the value
+      elif (not repeatingValues):
+         decompressedFile.write(data + "\n")
+
+      # Otherwise, if the line does contain repeating values, then we want to
+      # print out the value the number of times it repeats
+      elif (repeatingValues):
+
+         # Split the line into three parts: number of repeating values, the data,
+         # and the time the data was read
+         lineSplit = line.split()
+
+         # If the line didn't have 3 sections, then don't write anything
+         if (len(lineSplit) == 3):
+            numValues = int(lineSplit[0])
             data = lineSplit[1]
             dataTime = lineSplit[2]
 
-            # Now for each of the repeating values, write the data, along with the
-            # timestamp, to the file
-            for i in range(0, repeatingValues):
-                decompressedFile.write(data + "," + dataTime + "\n")
+         # Write the first value with a time stamp
+         decompressedFile.write(data + "," + dataTime + "\n")
 
-    compressedFile.close()
-    decompressedFile.close()
+         # Now, for the remaining repeated values, just write the value by itself
+         for i in range(1, numValues):
+            decompressedFile.write(data + "\n")
+
+   compressedFile.close()
+   decompressedFile.close()
 
 
 if __name__ == "__main__":
